1871a0e1b52781cb2918defbaab4e23355973b5f
Julian Squires <tek@wiw.org>
2001-06-22 02:48:11 +0000

    Numerous blit fixes, also x86 _blit162 is partially complete.

--- src/blit_common.c
+++ src/blit_common.c
@@ -1,7 +1,7 @@
 /* 
  * blit_common.c
  * Created: Mon Jan 29 13:42:41 2001 by tek@wiw.org
- * Revised: Sat May  5 16:34:27 2001 by tek@wiw.org
+ * Revised: Thu Jun 21 00:27:06 2001 by tek@wiw.org
  * Copyright 2001 Julian E. C. Squires (tek@wiw.org)
  * This program comes with ABSOLUTELY NO WARRANTY.
  * $Id$
@@ -11,34 +11,10 @@
 #include <dentata/types.h>
 #include <dentata/image.h>
 #include <dentata/error.h>
+#include "internal.h"
 
 typedef void(*blitfunction_t)(byte *, byte *, byte *, byte *, dword, dword,
                               dword, dword, dword, dword);
-
-extern void _blit80(byte *, byte *, byte *, byte *, dword, dword, dword,
-                    dword, dword, dword);
-extern void _blit81(byte *, byte *, byte *, byte *, dword, dword, dword,
-                    dword, dword, dword);
-extern void _blit160(byte *, byte *, byte *, byte *, dword, dword, dword,
-                     dword, dword, dword);
-extern void _blit161(byte *, byte *, byte *, byte *, dword, dword, dword,
-                     dword, dword, dword);
-extern void _blit162(byte *, byte *, byte *, byte *, dword, dword, dword,
-                     dword, dword, dword);
-extern void _blit164(byte *, byte *, byte *, byte *, dword, dword, dword,
-                     dword, dword, dword);
-extern void _blit168(byte *, byte *, byte *, byte *, dword, dword, dword,
-                     dword, dword, dword);
-extern void _blit240(byte *, byte *, byte *, byte *, dword, dword, dword,
-                     dword, dword, dword);
-extern void _blit241(byte *, byte *, byte *, byte *, dword, dword, dword,
-                     dword, dword, dword);
-extern void _blit242(byte *, byte *, byte *, byte *, dword, dword, dword,
-                     dword, dword, dword);
-extern void _blit244(byte *, byte *, byte *, byte *, dword, dword, dword,
-                     dword, dword, dword);
-extern void _blit248(byte *, byte *, byte *, byte *, dword, dword, dword,
-                     dword, dword, dword);
 
 #define NBPPS   4
 #define NALPHAS 5
@@ -74,6 +50,7 @@ void d_image_blit(d_image_t *d, d_image_t *s, d_point_t p)
         break;
     case 15:
         i = 1;
+        break;
     case 16:
         i = 2;
         break;
--- src/internal.h
+++ src/internal.h
@@ -1,7 +1,7 @@
 /* 
  * internal.h
  * Created: Sat Apr 28 16:29:17 2001 by tek@wiw.org
- * Revised: Sat Apr 28 16:29:43 2001 by tek@wiw.org
+ * Revised: Thu Jun 21 00:19:21 2001 by tek@wiw.org
  * Copyright 2001 Julian E. C. Squires (tek@wiw.org)
  * This program comes with ABSOLUTELY NO WARRANTY.
  * $Id$
@@ -13,5 +13,29 @@
 
 extern void __d_image_clip(d_image_t *, d_image_t *, d_point_t, dword *,
                            dword *, dword *, dword *, dword *, dword *);
+extern void _blit80(byte *, byte *, byte *, byte *, dword, dword, dword,
+                    dword, dword, dword);
+extern void _blit81(byte *, byte *, byte *, byte *, dword, dword, dword,
+                    dword, dword, dword);
+extern void _blit160(byte *, byte *, byte *, byte *, dword, dword, dword,
+                     dword, dword, dword);
+extern void _blit161(byte *, byte *, byte *, byte *, dword, dword, dword,
+                     dword, dword, dword);
+extern void _blit162(byte *, byte *, byte *, byte *, dword, dword, dword,
+                     dword, dword, dword);
+extern void _blit164(byte *, byte *, byte *, byte *, dword, dword, dword,
+                     dword, dword, dword);
+extern void _blit168(byte *, byte *, byte *, byte *, dword, dword, dword,
+                     dword, dword, dword);
+extern void _blit240(byte *, byte *, byte *, byte *, dword, dword, dword,
+                     dword, dword, dword);
+extern void _blit241(byte *, byte *, byte *, byte *, dword, dword, dword,
+                     dword, dword, dword);
+extern void _blit242(byte *, byte *, byte *, byte *, dword, dword, dword,
+                     dword, dword, dword);
+extern void _blit244(byte *, byte *, byte *, byte *, dword, dword, dword,
+                     dword, dword, dword);
+extern void _blit248(byte *, byte *, byte *, byte *, dword, dword, dword,
+                     dword, dword, dword);
 
 /* EOF internal.h */
